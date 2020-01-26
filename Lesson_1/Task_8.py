838e4b0a762399b282ee54d4dad18c06a2d65668
Ruslan Smirnov <smirnov_ruslan@outlook.com>
2020-01-26 11:18:21 +0300

    Add 8th task

---
+++ Lesson_1/Task_8.py
@@ -0,0 +1,13 @@
+from collections import Counter
+
+
+def max_3(dict_):
+    counter = Counter(dict_)
+    return counter.most_common(3)
+
+
+dict_ = {'a': 11, 'b': 44, 'c': 5,      # maxes = [50, 44, 11]
+            'd': 9, 'e': 50, 'f': 10,
+            'g': 7, 'h':7} 
+
+print(f"{max_3(dict_)}")
