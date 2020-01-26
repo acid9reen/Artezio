5f05fa864390a1d58a8d4bdf05225adfa179a1e6
Ruslan Smirnov <smirnov_ruslan@outlook.com>
2020-01-27 01:18:06 +0300

    Fix codestyle

--- Lesson_1/Task_7.py
+++ Lesson_1/Task_7.py
@@ -3,8 +3,8 @@ def merge(dict_1, dict_2):
 
 
 dict_1 = {'a': 11, 'b': 44, 'c': 5,   
-    'd': 9, 'e': 50, 'f': 10,}
+    'd': 9, 'e': 50, 'f': 10, }
 
-dict_2 = {'g': 7, 'h':7} 
+dict_2 = {'g': 7, 'h':7, } 
 
 print(merge(dict_1, dict_2))
