518d3b7b32580b20e8a12f16b4ce428256cbdbe0
Ruslan Smirnov <smirnov_ruslan@outlook.com>
2020-01-28 20:58:48 +0300

    Fix codestyle issues

--- Lesson_1/Task_04.py
+++ Lesson_1/Task_04.py
@@ -1,10 +1,10 @@
 def special_string_counter(list_):
     counter = 0
-    
+
     for str_ in list_:
         if(len(str_) > 1 and str_[:1] == str_[-1:]):
             counter += 1
-    
+
     return counter
 
 
