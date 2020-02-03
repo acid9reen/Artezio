9e0620ee062455ccf17570211c185ccef95bc2aa
Ruslan Smirnov <smirnov_ruslan@outlook.com>
2020-02-03 14:32:02 +0300

    Use function variable instead of global

--- Lesson_3/Task_3.py
+++ Lesson_3/Task_3.py
@@ -1,16 +1,15 @@
-g_a = None
-
-
 def average(a, b, c, d):
     list_ = [a, b, c, d]
-    global g_a
 
-    if g_a is None:
-        g_a = max(list_)
+    if average.max_ is None:
+        average.max_ = max(list_)
     else:
-        g_a = g_a if g_a > (max_ := max(list_)) else max_
+        average.max_ = average.max_ if average.max_ > (max_ := max(list_)) else max_
 
-    return sum(list_) / 4, g_a
+    return sum(list_) / 4, average.max_
+
+
+average.max_ = None
 
 
 def main():
