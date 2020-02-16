c5500a026e114b7b6b9290d6fb177a46f485f61d
Ruslan Smirnov <smirnov_ruslan@outlook.com>
2020-02-16 14:52:22 +0300

    Add 2nd task

---
+++ Lesson_7/task2.py
@@ -0,0 +1,79 @@
+'''Time_methods decorator'''
+
+import time
+
+
+def time_this(func):
+    '''Function decorator for time calculation'''
+
+    def wrapper(*args, **kwargs):
+
+        if args and kwargs:
+            time_1 = time.time()
+            func(args, kwargs)
+            time_2 = int((time.time() - time_1) * 1000)
+        else:
+            time_1 = time.time()
+            func()
+            time_2 = int((time.time() - time_1) * 1000)
+
+        print(f"'{func.__name__}' method running time: {time_2} ms")
+
+    return wrapper
+
+
+def time_methods(*methods):
+    '''Take decorator arguments'''
+
+    def decorator(cls):
+        '''Ckass decorator that calculate
+            given method's running time'''
+
+        class WrapperClass:
+            '''Wrapper class'''
+
+            def __init__(self, *args, **kwargs):
+                self.o_instance = cls(*args, **kwargs)
+
+            def __getattr__(self, item):
+                method = self.o_instance.__getattribute__(item)
+
+                if method.__name__ in methods:
+                    return time_this(method)
+
+                return method
+
+        return WrapperClass
+
+    return decorator
+
+
+@time_methods('inspect', 'bar')
+class Spam:
+    '''Some class'''
+
+    def __init__(self, sec):
+        self.sec = sec
+
+    def inspect(self):
+        '''Some method'''
+
+        time.sleep(self.sec)
+
+    def some_method(self):
+        '''Another some method'''
+
+        return self.sec
+
+
+def main():
+    '''Create class object
+        and calls it's methods'''
+
+    obj = Spam(3)
+    obj.some_method()
+    obj.inspect()
+
+
+if __name__ == '__main__':
+    main()
